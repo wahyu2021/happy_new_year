82e954237265cc868aec77acf747b57a6d7700a3
wahyu2021 <wahyuwn256@gmail.com>
2023-12-31 10:52:41 +0700

    selesai

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.hmtl",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
